54fbac92cf4a6f03f450f61b5e0264ec24164c63
Rafi-Shariar <rafi.shariar619@gmail.com>
2026-03-07 21:32:12 +0600

    login form added

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -5,25 +5,29 @@ import LogoHolder from "../pages/shared/LogoHolder";
 
 const AuthLayout = () => {
   return (
-    <div className=" bg-base-200 min-h-screen">
+    <div className="bg-base-200 min-h-screen">
+      <div className="flex flex-col lg:flex-row min-h-screen">
 
-      <div className="flex">
+        {/* Left */}
+        <div className="flex-1 flex flex-col">
 
-        <div className="flex-1">
-           
-           <div className="p-12">
-             <LogoHolder />
-           </div>
-           
-           <div className="max-w-3xl mx-auto">
+          <div className="p-8 lg:p-12">
+            <LogoHolder />
+          </div>
+
+          <div className="flex-1 flex justify-center items-center px-6">
+            <div className="w-full max-w-md">
               <Outlet />
-           </div>
+            </div>
+          </div>
+
         </div>
 
-        <div className="bg-green-50 flex-1 min-h-screen flex justify-center items-center">
-          <img src={authImage} className="" />
+        {/* Right */}
+        <div className="flex-1 bg-green-50 flex justify-center items-center p-10">
+          <img src={authImage} className="max-w-md w-full" alt="auth" />
         </div>
-        
+
       </div>
     </div>
   );

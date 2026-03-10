197e8e75d21105ee4ae315452136123ede6b9ccd
Rafi-Shariar <rafi.shariar619@gmail.com>
2026-03-10 23:47:41 +0600

    auth function setup

--- src/context/authContext/AuthProvider.jsx
+++ src/context/authContext/AuthProvider.jsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
+import { auth } from '../../firebase/firebase.init';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 
 const AuthProvider = ({children}) => {
 
-    const authInfo = {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
+    //creating a user
+    const createUser = (email,password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth,email,password);
+    }
+
+    const singIn = (email,password) => {
+        return signInWithEmailAndPassword(auth,email,password);
+        
+    }
+
+    const logOut = () =>{
+        setLoading(true);
+        return signOut(auth);
+    }
+    
+    //Observer
+    useEffect(()=>{
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            setLoading(false);
+        });
+
+        return () =>{
+            unSubscribe();
+        }
+    },[])
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        singIn,
+        logOut
     }
     return (
        <AuthContext value={authInfo}>
